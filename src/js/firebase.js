887bb073c478f7087e5764b27487e9094abdbf81
Jermaine <64845752+Codemaine@users.noreply.github.com>
2020-12-12 12:23:43 +0000

    Update firebase.js

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -3,12 +3,14 @@
 import * as firebase from "firebase";
 
 var config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyD0Iorrgyx7hpVuMLs1um3GcBJvsZJBdB8",
+  authDomain: "fistbump-b9aaa.firebaseapp.com",
+  databaseURL: "https://fistbump-b9aaa.firebaseio.com",
+  projectId: "fistbump-b9aaa",
+  storageBucket: "fistbump-b9aaa.appspot.com",
+  messagingSenderId: "423661765169",
+  appId: "1:423661765169:web:e8188e7300ba6a5ad20b10",
+  measurementId: "G-3W732J6LHD"
   };
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
